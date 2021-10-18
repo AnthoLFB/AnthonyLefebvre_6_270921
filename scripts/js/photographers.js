cdc9b0c87b82176a538a8cae309887b1aca77d6d
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-10-18 20:08:31 +0200

    Ajout de la fonction asynchrone permettant d'afficher les photographes

--- scripts/js/photographers.js
+++ scripts/js/photographers.js
@@ -52,4 +52,79 @@ async function renderPhotographersTags()
     }
 }
 
+async function renderPhotographersList() 
+{
+    //Appel de la classe 
+    let recoveredData = new dataRecovery("photographers.json");
+    
+    //Récupération des données du JSON
+    recoveredData = await recoveredData.getData();
+
+    if(recoveredData === undefined)
+    {
+        let htmlSegment = `
+                            <article class="main__contenu__photographer-card">
+
+                                <p class="main__contenu__photographer-card__error">Aucun photographe n'a été trouvé.</p>
+
+                            </article>
+        `;
+
+        //Récupération du container
+        let container = document.querySelector('.main__contenu');
+
+        //Ajout des éléments HTML
+        container.innerHTML = htmlSegment;
+    }
+    else
+    {
+        //Déclaration des variables utiles
+        let html = [];
+
+        //Boucle sur les resultats de la requête
+        recoveredData.photographers.forEach(photographer => {
+        
+            //Création de l'élément HTML avec le tag concerné
+            let htmlSegment = `
+                            <article class="main__contenu__photographer-card">
+
+                                <a class="main__contenu__photographer-card__link" href="#">
+                                    <img class="main__contenu__photographer-card__link__image" src="images/photographers/photographersProfilPicture/${photographer.portrait}" alt="Photo de profil pour le photographe :  ${photographer.name}"/>
+                                    <h2 class="main__contenu__photographer-card__link__title">${photographer.name}</h2>
+                                </a>
+
+                                <p class="main__contenu__photographer-card__catchphrase">${photographer.tagline}</p>
+
+                                <ul class="main__contenu__photographer-card__tag-list">
+            `;
+
+            //Boucle sur chaques tags du photographe
+            photographer.tags.forEach(tag =>
+            {      
+                //Création de l'élément HTML avec le tag concerné
+                htmlSegment += `<li class="main__contenu__photographer-card__tag-list__item"><a class="main__contenu__photographer-card__tag-list__item__link" href="#">${tag}</a></li>`;
+            });
+            
+            //Fin de l'article
+            htmlSegment += `
+                                
+                                </ul>
+
+                            </article>
+            `;
+
+            //Enregistrement de la ligne
+            html += htmlSegment;
+    
+        });
+
+        //Récupération du container
+        let container = document.querySelector('.main__contenu');
+
+        //Ajout des éléments HTML
+        container.innerHTML = html;
+    }
+}
+
 renderPhotographersTags();
+renderPhotographersList();
