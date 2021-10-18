f3bfcf79c6b8c9dc5c96df8a19c8cee74c95b504
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-10-18 17:46:30 +0200

    Création d'une classe permettant de récupérer les données d'un fichier JSON donné.

---
+++ scripts/js/dataRecovery.js
@@ -0,0 +1,27 @@
+class dataRecovery
+{
+    constructor(jsonFileName)
+    {
+        this.jsonFileName = jsonFileName;
+    }
+
+    getData()
+    {
+        //Création de la requête et récupération du résultat dans une variable
+        let requestResult = 
+            fetch("scripts/js/" + this.jsonFileName)
+            .then(function(res) 
+            {
+                if (res.ok) 
+                {
+                    return res.json();
+                }
+            })
+            .catch(function(err) {
+                console.log("Une erreur est survenue : " + err);
+            });
+
+        //Renvoie du resultat
+        return requestResult;
+    }
+}
