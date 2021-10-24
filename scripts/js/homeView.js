6b7c2ac8e5198c7bdbf332839b433249de6cb473
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-10-24 21:33:10 +0200

    Ajout des méthodes permettant d'afficher les photographes en HTML

--- scripts/js/homeView.js
+++ scripts/js/homeView.js
@@ -34,8 +34,147 @@ class HomeView
 
             //Ajout des éléments HTML
             container.innerHTML = htmlSegment;
+
+            new Tag().addEventOnTags("header__navBar__list__item__link");
         }
-    }    
+    }
+
+    async allPhotographersRender()
+    {
+        //Appel et récupération des données
+        let photographerRecovery = new Photographer()
+        let photographersArray = await photographerRecovery.getPhotographers();
+
+        let htmlSegment = "";
+
+        if(photographersArray === undefined)
+        {
+            let htmlSegment = `
+                            <article class="main__contenu__photographer-card">
+
+                                <p class="main__contenu__photographer-card__error">Aucun photographe n'a été trouvé.</p>
+
+                            </article>
+            `;
+
+            //Récupération du container
+            let container = document.querySelector('.main__contenu');
+
+            //Ajout des éléments HTML
+            container.innerHTML = htmlSegment;
+        }
+        else
+        {
+            photographersArray.forEach(photographer =>
+            {
+
+                htmlSegment += `
+                                <article class="main__contenu__photographer-card">
+
+                                    <a class="main__contenu__photographer-card__link" href="#">
+                                        <img class="main__contenu__photographer-card__link__image" src="images/photographers/photographersProfilPicture/${photographer.portrait}" alt="Photo de profil pour le photographe :  ${photographer.name}"/>
+                                        <h2 class="main__contenu__photographer-card__link__title">${photographer.name}</h2>
+                                    </a>
+
+                                    <p class="main__contenu__photographer-card__catchphrase">${photographer.tagline}</p>
+
+                                    <ul class="main__contenu__photographer-card__tag-list">
+                `;
+
+                //Boucle sur chaques tags du photographe
+                photographer.tags.forEach(tag =>
+                {      
+                    //Création de l'élément HTML avec le tag concerné
+                    htmlSegment += `<li class="main__contenu__photographer-card__tag-list__item"><a class="main__contenu__photographer-card__tag-list__item__link" href="#">#${tag}</a></li>`;
+                });
+                
+                //Fin de l'article
+                htmlSegment += `
+                                    
+                                    </ul>
+
+                                </article>
+                `;
+            });
+
+            //Récupération du container
+            let container = document.querySelector('.main__contenu');
+
+            //Ajout des éléments HTML
+            container.innerHTML = htmlSegment;
+
+            new Tag().addEventOnTags("main__contenu__photographer-card__tag-list__item__link");
+        }
+    }
+    
+    async filteredPhotographersRender(filter)
+    {
+        //Appel et récupération des données
+        let photographerRecovery = new Photographer()
+        let photographersArray = await photographerRecovery.getPhotographers(filter.substr(1));
+
+        let htmlSegment = "";
+
+        if(photographersArray === undefined)
+        {
+            let htmlSegment = `
+                            <article class="main__contenu__photographer-card">
+
+                                <p class="main__contenu__photographer-card__error">Aucun photographe n'a été trouvé.</p>
+
+                            </article>
+            `;
+
+            //Récupération du container
+            let container = document.querySelector('.main__contenu');
+
+            //Ajout des éléments HTML
+            container.innerHTML = htmlSegment;
+        }
+        else
+        {
+            photographersArray.forEach(photographer =>
+            {
+
+                htmlSegment += `
+                                <article class="main__contenu__photographer-card">
+
+                                    <a class="main__contenu__photographer-card__link" href="#">
+                                        <img class="main__contenu__photographer-card__link__image" src="images/photographers/photographersProfilPicture/${photographer.portrait}" alt="Photo de profil pour le photographe :  ${photographer.name}"/>
+                                        <h2 class="main__contenu__photographer-card__link__title">${photographer.name}</h2>
+                                    </a>
+
+                                    <p class="main__contenu__photographer-card__catchphrase">${photographer.tagline}</p>
+
+                                    <ul class="main__contenu__photographer-card__tag-list">
+                `;
+
+                //Boucle sur chaques tags du photographe
+                photographer.tags.forEach(tag =>
+                {      
+                    //Création de l'élément HTML avec le tag concerné
+                    htmlSegment += `<li class="main__contenu__photographer-card__tag-list__item"><a class="main__contenu__photographer-card__tag-list__item__link" href="#">#${tag}</a></li>`;
+                });
+                
+                //Fin de l'article
+                htmlSegment += `
+                                    
+                                    </ul>
+
+                                </article>
+                `;
+            });
+
+            //Récupération du container
+            let container = document.querySelector('.main__contenu');
+
+            //Ajout des éléments HTML
+            container.innerHTML = htmlSegment;
+
+            new Tag().addEventOnTags("main__contenu__photographer-card__tag-list__item__link");
+        }
+    }
 }
 
 new HomeView().tagsRender();
+new HomeView().allPhotographersRender();
