10aa4dbe64b4627794f024f53317592e71718ed1
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-10-24 21:33:39 +0200

    Ajout des méthodes permettant de traiter les données relatives aux photographes

--- scripts/js/photographers.js
+++ scripts/js/photographers.js
@@ -1,3 +1,105 @@
+class Photographer
+{
+    constructor(name, id, city, country, tags, tagline, price, portrait)
+    {
+        this.name = name;
+        this.id = id;
+        this.city = city;
+        this.country = country;
+        this.tags = tags;
+        this.tagline = tagline;
+        this.price = price;
+        this.portrait = portrait
+    }
+
+    async getPhotographers(filter = "all")
+    {
+        //Appel de la classe 
+        let recoveredData = new dataRecovery("photographers.json");
+            
+        //Récupération des données du JSON
+        recoveredData = await recoveredData.getData();
+
+        //Déclaration des variables utiles
+        let photographersArray = new Array();
+
+        if(filter == "all")
+        {
+            //Boucle sur les resultats de la requête
+            recoveredData.photographers.forEach(photographer => {
+
+                //Création d'un objet photographe
+                let photographerObject = new Photographer(photographer.name, photographer.id, photographer.city, photographer.country, photographer.tags, photographer.tagline ,photographer.price, photographer.portrait);
+
+                //Stockage dans un tableau
+                photographersArray.push(photographerObject);
+            });
+
+            return photographersArray;
+        }
+        else
+        {
+            //Boucle sur les resultats de la requête
+            recoveredData.photographers.forEach(photographer => {
+
+                photographer.tags.forEach(tag =>
+                {
+                    if(tag == filter)
+                    {
+                        //Création d'un objet photographe
+                        let photographerObject = new Photographer(photographer.name, photographer.id, photographer.city, photographer.country, photographer.tags, photographer.tagline ,photographer.price, photographer.portrait);
+
+                        //Stockage dans un tableau
+                        photographersArray.push(photographerObject);
+                    }
+                });
+            });
+
+            return photographersArray;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 async function renderPhotographersTags() 
 {
     //Appel de la classe 
@@ -128,3 +230,4 @@ async function renderPhotographersList()
 
 renderPhotographersTags();
 renderPhotographersList();
+*/
