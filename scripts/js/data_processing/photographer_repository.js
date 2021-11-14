3339c379a02db1f6de87d3d9df033e735ad99fd6
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2021-11-14 16:06:02 +0100

    Modification des rpository existants, ajout de media repository (à modifier)

--- scripts/js/data_processing/photographer_repository.js
+++ scripts/js/data_processing/photographer_repository.js
@@ -1,17 +1,18 @@
-class PhotographerRepository extends DataRecovery 
+class PhotographerRepository extends DataRepository 
 {
     findAll() 
     {
         //Permet de récupérer tous les photographes et de les retourner dans un tableau "photographers" sous forme d'objet photographe.
         return this.getData()
             .then((data) => data.photographers.map((photographer) => new Photographer(
-            photographer.name,
-            photographer.id,
-            photographer.city,
-            photographer.country,
-            photographer.tags,
-            photographer.tagline,
-            photographer.price, photographer.portrait
+                photographer.name,
+                photographer.id,
+                photographer.city,
+                photographer.country,
+                photographer.tags,
+                photographer.tagline,
+                photographer.price, 
+                photographer.portrait
         )));
     }
 }
